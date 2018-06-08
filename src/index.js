6b2540bfb310a573084ceb0b3870347e5793e91e
Christian Vaagan <cvagan@gmail.com>
2018-06-08 15:10:46 +0200

    prepare deployment

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import { changeInput, changeBox, setScores, getUser, getRoute } from "./reducers";
 import thunkMiddleware from "redux-thunk";
-import { createLogger } from "redux-logger";
 import './index.css';
 import App from './App';
 import "tachyons";
 import registerServiceWorker from './registerServiceWorker';
 
-const logger = createLogger();
 const rootReducer = combineReducers({changeInput, changeBox, setScores, getUser, getRoute});
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
 	<Provider store={store}>
